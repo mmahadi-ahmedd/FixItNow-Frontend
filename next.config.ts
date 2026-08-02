5b232364b96a0803651c8393e2875e4268741067
mmahadi-ahmedd <mahadiahmed13550@gmail.com>
2026-08-02 09:17:46 +0600

    fix: proxy backend through Next.js rewrites to fix cross-origin cookie issue

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+ async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://fixitnow-backend-eosin.vercel.app/api/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
